11c28d8975b54d7ede921c6f72663e3de0a1208f
Rajvinder Singh <anonymous172q@gmail.com>
2024-12-28 15:13:07 +0000

    Update ProfilePage.jsx

--- chat-app/frontend/src/pages/ProfilePage.jsx
+++ chat-app/frontend/src/pages/ProfilePage.jsx
@@ -32,7 +32,7 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={selectedImg || authUser.user.profilePic || "/avatar.png"}
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
@@ -68,7 +68,7 @@ const ProfilePage = () => {
                 <User className="w-4 h-4" />
                 Full Name
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser.user.fullName}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser.fullName}</p>
             </div>
 
             <div className="space-y-1.5">
@@ -76,7 +76,7 @@ const ProfilePage = () => {
                 <Mail className="w-4 h-4" />
                 Email Address
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser.user.email}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser.email}</p>
             </div>
           </div>
 
@@ -85,7 +85,7 @@ const ProfilePage = () => {
             <div className="space-y-3 text-sm">
               <div className="flex items-center justify-between py-2 border-b border-zinc-700">
                 <span>Member Since</span>
-                <span>{authUser.user.createdAt?.split("T")[0]}</span>
+                <span>{authUser.createdAt?.split("T")[0]}</span>
               </div>
               <div className="flex items-center justify-between py-2">
                 <span>Account Status</span>
