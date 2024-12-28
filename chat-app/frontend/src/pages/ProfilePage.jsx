4823a6143f16d1c368f6e589faebeaf3d93eac3a
Rajvinder Singh <anonymous172q@gmail.com>
2024-12-28 15:25:53 +0000

    Update ProfilePage.jsx

--- chat-app/frontend/src/pages/ProfilePage.jsx
+++ chat-app/frontend/src/pages/ProfilePage.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
     reader.onload = async () => {
       const base64Image = reader.result;
       console.log(base64Image);
-      await updateProfile({ profileImage: base64Image });
+      await updateProfile({ profilePic: base64Image });
     };
   };
   return (
