b5c03c713c9bbaa696153596700ca51dec5d7dd8
Rajvinder Singh <anonymous172q@gmail.com>
2024-12-28 15:20:47 +0000

    Update SignUp.jsx

--- chat-app/frontend/src/pages/SignUp.jsx
+++ chat-app/frontend/src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ const SignUpPage = () => {
   const { signup, isSigningUp } = useAuthStore();
 
   const validateForm = () => {
-    if (!formData.name.trim()) return toast.error("Full name is required");
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
     if (!formData.email.trim()) return toast.error("Email is required");
     if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
     if (!formData.password) return toast.error("Password is required");
