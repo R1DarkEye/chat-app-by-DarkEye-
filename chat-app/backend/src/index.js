615686a1ce196878e7a2030a85cf2ac2b39d90fb
Rajvinder Singh <anonymous172q@gmail.com>
2024-12-28 14:39:19 +0000

    Update index.js

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -1,35 +1,42 @@
-import authRoutes from '../routes/auth.Routes.js';
-import messageRoutes from '../routes/message.Routes.js';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import connectDB from '../libs/db.js';
-import cors from 'cors';
-import express from 'express';
-import path from 'path';
-import {app,server} from '../libs/socket.js'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+import path from "path";
 
-dotenv.config()
-app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
+import { app, server } from "../libs/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use('/api/auth',authRoutes)
-app.use('/api/messages',messageRoutes)
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../frontend/dist')))
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
-    })}
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
-    console.log('Server is running on port',PORT);
-    connectDB();
-    });
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
